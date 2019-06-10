274d47a86d4d4db6726457986ba34982a92b1067
sbubuntu <soumitra05334@gmail.com>
2019-06-10 14:21:07 +0200

    Updated kafkatobq.java

--- kafkatobq.java
+++ kafkatobq.java
@@ -1,6 +1,12 @@
 package com.google.cloud.training.dataanalyst.javahelp;
 import java.time.Instant;
 import java.util.ArrayList;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.LongDeserializer;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.List;
 import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;
 import org.apache.beam.sdk.Pipeline;
@@ -13,18 +19,23 @@ import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.transforms.Sum;
 import org.apache.beam.sdk.transforms.windowing.SlidingWindows;
+import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.transforms.Values;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.coders.StringUtf8Coder;
+import com.google.common.collect.ImmutableMap;
 import org.apache.beam.sdk.transforms.windowing.Window;
 import org.joda.time.Duration;
 import com.google.api.services.bigquery.model.TableFieldSchema;
 import com.google.api.services.bigquery.model.TableRow;
+import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.PCollection;
 import com.google.api.services.bigquery.model.TableSchema;
 
-public class StreamDemoConsumer {
+public class kafkatobq {
 
 	public static interface MyOptions extends DataflowPipelineOptions {
-
-		@Description("Output BigQuery table <project_id>:<dataset_id>.<table_id>")
-
+	@Description("Output BigQuery table <project_id>:<dataset_id>.<table_id>")
 		@Default.String("cloud-training-demos:demos.streamdemo2")
 		String getOutput();
 		void setOutput(String s);
@@ -32,7 +43,6 @@ public class StreamDemoConsumer {
 		@Default.String("projects/cloud-training-demos/topics/streamdemo")
 		String getInput();
 		void setInput(String s);
-
 	}
 	@SuppressWarnings("serial")
 	public static void main(String[] args) {
@@ -41,35 +51,25 @@ public class StreamDemoConsumer {
 		Pipeline p = Pipeline.create(options);
 		String topic = options.getInput();
 		String output = options.getOutput();
-
+		Map<String, Object> propertyBuilder = new HashMap();
+        propertyBuilder.put("request.timeout.ms",20000);
+        propertyBuilder.put("retry.backoff.ms",500);
 		// Build the table schema for the output table.
-
 		List<TableFieldSchema> fields = new ArrayList<>();
 		fields.add(new TableFieldSchema().setName("NAME").setType("STRING"));
 		fields.add(new TableFieldSchema().setName("ADDRESS").setType("STRING"));
 		fields.add(new TableFieldSchema().setName("ID").setType("STRING"));
 		fields.add(new TableFieldSchema().setName("ACC_NO").setType("STRING"));
 		TableSchema schema = new TableSchema().setFields(fields);
-		
-		KafkaIO.Read<byte[], String> kafkaIOReader = KafkaIO.read()
-        .withBootstrapServers("192.168.99.100:32771")
-        .withTopics(Arrays.asList("beam".split(",")))
-        .updateConsumerProperties(ImmutableMap.of("auto.offset.reset", (Object)"earliest"))
-        .withValueCoder(StringUtf8Coder.of());
 		p //
-				.apply("GetMessages", kafkaIOReader.withoutMetadata()) //
-				.apply("window",
-						Window.into(SlidingWindows//
-								.of(Duration.standardMinutes(2))//
-								.every(Duration.standardSeconds(30)))) //
-				.apply("WordsPerLine", ParDo.of(new DoFn<String, Integer>() {
-					@ProcessElement
-					public void processElement(ProcessContext c) throws Exception {
-						String line = c.element();
-						c.output(line.split(" ").length);
-					}
-				}))//
-					
+				.apply(KafkaIO.<Long, String>read()
+    			        	.withBootstrapServers("35.228.77.245:9092")
+      			       		.withTopic("users")  // use withTopics(List<String>) to read from multiple topics.
+      			        	.withKeyDeserializer(LongDeserializer.class)
+    			        	.withValueDeserializer(StringDeserializer.class)
+					.withoutMetadata()
+				     )
+				.apply(Values.<String>create())
 				.apply("MapToTableRow", ParDo.of(new DoFn<String, TableRow>() {
 					@ProcessElement
 					public void processElement(ProcessContext c) { 
@@ -83,8 +83,6 @@ public class StreamDemoConsumer {
 						c.output(row);
 						}
 					}))
-				
-					
 				.apply(BigQueryIO.writeTableRows().to(output)//
 						.withSchema(schema)//
 						.withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_APPEND)
